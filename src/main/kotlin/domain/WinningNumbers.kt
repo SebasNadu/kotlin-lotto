4f686df331ba8e17701b4f7ba0ec29378d90663e
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 17:19:03 +0200

    refact(domain): better parameters naming

--- src/main/kotlin/domain/WinningNumbers.kt
+++ src/main/kotlin/domain/WinningNumbers.kt
@@ -1,8 +1,8 @@
 package domain
 
-class WinningNumbers(val winningNumbers: Lotto, val bonusNumber: Int = 0) {
+class WinningNumbers(val winningTicket: Lotto, val bonusNumber: Int = 0) {
         init {
             require(bonusNumber in 1..45)
-            require(winningNumbers.numbers.none { it == bonusNumber })
+            require(winningTicket.numbers.none { it == bonusNumber })
         }
 }

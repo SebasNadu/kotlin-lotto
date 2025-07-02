c980a505a8e3aa6a1fed392669ebe56cf88c400a
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 14:56:25 +0200

    feat(service): add generateLottoTickets() method

--- src/main/kotlin/services/LottoService.kt
+++ src/main/kotlin/services/LottoService.kt
@@ -1,7 +1,29 @@
 package services
 
+import domain.Lotto
+import kotlin.random.Random
+
 object LottoService {
     fun purchaseAmountValidator(amount: Int): Boolean {
         return amount >= 1000 && amount % 1000 == 0
+    }
+
+    fun generateLottoTickets(amount: Int): List<Lotto> {
+        val numberTickets = amount / 1000
+        val lottoTickets: MutableList<Lotto> = mutableListOf()
+        repeat(numberTickets) {
+            val numbers = generateRandomNumbers(6)
+            lottoTickets.add(Lotto(numbers))
+        }
+        return lottoTickets
+    }
+
+    fun generateRandomNumbers(size: Int): List<Int> {
+        val numbers = mutableListOf<Int>()
+        for (i in 1..45) {
+            numbers.add(i)
+        }
+        numbers.shuffle()
+        return numbers.subList(0, size)
     }
 }

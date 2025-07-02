a460b8768d80d91941ef3d9025342b1adead1fab
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 18:09:38 +0200

    refactor: use constants to remove magic variables

--- src/main/kotlin/domain/WinningNumbers.kt
+++ src/main/kotlin/domain/WinningNumbers.kt
@@ -2,7 +2,7 @@ package domain
 
 class WinningNumbers(val winningTicket: Lotto, val bonusNumber: Int = 0) {
         init {
-            require(bonusNumber in 1..45)
+            require(bonusNumber in Lotto.MIN_SIZE..Lotto.MAX_RANGE)
             require(winningTicket.numbers.none { it == bonusNumber })
         }
 }

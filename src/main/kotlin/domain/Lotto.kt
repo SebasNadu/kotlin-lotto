973032742ad221c8fb8b47867582f527463742c0
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 09:46:10 +0200

    feat(domain): add range validation into Lotto

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -4,5 +4,6 @@ class Lotto(val numbers: List<Int> = emptyList<Int>()) {
     init {
         require(numbers.size == 6)
         require(numbers.distinct().size == 6)
+        require(numbers.all { it in 1..45 })
     }
 }

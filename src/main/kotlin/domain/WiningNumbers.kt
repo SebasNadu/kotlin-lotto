9d3229121592f00f5404e9318241ed49fd3a2f7f
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 11:10:56 +0200

    refactor(domain): delete duplicated conditional validation in WinningNumbers class

--- src/main/kotlin/domain/WiningNumbers.kt
+++ src/main/kotlin/domain/WiningNumbers.kt
@@ -3,7 +3,6 @@ package domain
 class WiningNumbers(winningNumbers: List<Int> = listOf(), val bonusNumber: Int = 0) {
     val ticket: Lotto = Lotto(winningNumbers)
         init {
-            require(bonusNumber != 0)
             require(bonusNumber in 1..45)
             require(ticket.numbers.none { it == bonusNumber })
         }

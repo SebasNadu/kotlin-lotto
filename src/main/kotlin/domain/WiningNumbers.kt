71203257c8c0bef9c42c5e2d9e9413c5f75dcdc4
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 16:01:14 +0200

    refactor(service): WinningNumbers accept directly Lotto instance

--- src/main/kotlin/domain/WiningNumbers.kt
+++
@@ -1,9 +0,0 @@
-package domain
-
-class WiningNumbers(winningNumbers: List<Int> = listOf(), val bonusNumber: Int = 0) {
-    val ticket: Lotto = Lotto(winningNumbers)
-        init {
-            require(bonusNumber in 1..45)
-            require(ticket.numbers.none { it == bonusNumber })
-        }
-}

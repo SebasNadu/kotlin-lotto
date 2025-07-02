c083444cd0bab33374d4de839bdfe98080e3550a
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 16:02:51 +0200

    feat(controller): add logic to create WinningNumbers

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -1,6 +1,7 @@
 package controller
 
 import domain.Lotto
+import domain.WinningNumbers
 import services.LottoService
 import util.LottoUtils.retry
 import view.InputView
@@ -9,9 +10,15 @@ import view.OutputView
 class LottoController {
 
     fun run() {
-        val amount = retry(InputView::getPurchaseAmount, LottoService::purchaseAmountValidator)
+        val amount = retry({
+            InputView.getPurchaseAmount().also { LottoService.purchaseAmountValidator(it) }
+        })
         val lottoTickets = LottoService.generateLottoTickets(amount)
         OutputView.printTickets(lottoTickets)
+        val winningLotto: Lotto = retry { Lotto(InputView.getWinningNumbers()) }
+        val winningNumbers = retry {
+            WinningNumbers(winningLotto, InputView.getBonusNumber())
+        }
     }
 
 }

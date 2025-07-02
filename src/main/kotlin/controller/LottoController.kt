0fcbcfe8d2d48b998ef8a8844a24cbba5416ac58
Mina Kim <mina.kim@deliveryhero.com>
2025-07-02 15:08:58 +0200

    feat(controller): adapt new printTickets method inside OutputView

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -4,12 +4,14 @@ import domain.Lotto
 import services.LottoService
 import util.LottoUtils.retry
 import view.InputView
+import view.OutputView
 
 class LottoController {
 
     fun run() {
         val amount = retry(InputView::getPurchaseAmount, LottoService::purchaseAmountValidator)
         val lottoTickets = LottoService.generateLottoTickets(amount)
+        OutputView.printTickets(lottoTickets)
     }
 
 }

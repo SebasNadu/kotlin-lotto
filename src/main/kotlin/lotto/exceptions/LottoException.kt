88e693feb3377ce39e9facc2dde24864758c33be
Mina Kim <mina.kim@deliveryhero.com>
2025-07-03 15:42:19 +0200

    feat(exception): implement customize exceptions to simplify and centralize the error messages

--- src/main/kotlin/lotto/exceptions/LottoException.kt
+++ src/main/kotlin/lotto/exceptions/LottoException.kt
@@ -1,40 +1,23 @@
 package lotto.exceptions
 
 import lotto.domain.Lotto
-import lotto.domain.LottoMachine
-import lotto.domain.LottoNumber
 
 sealed class LottoException(msg: String) : IllegalArgumentException("[ERROR]: $msg") {
-    class InvalidAmountFormatException(input: String) : LottoException("Wrong amount format: $input.\n")
 
-    class InvalidAmountException(amount: Int) :
-        LottoException(
-            "Wrong amount: $amount, Amount should be at least ${LottoMachine.PRICE_OF_TICKET} ${LottoMachine.CURRENCY}" +
-                "and divisible by ${LottoMachine.PRICE_OF_TICKET}.\n",
-        )
+	class InvalidAmountFormatException(input: String) : LottoException("Wrong amount format: ${input}.\n")
 
-    class InvalidLottoNumbersException() : LottoException(
-        "Invalid lotto numbers: Expected ${Lotto.LOTTO_PICK_SIZE} unique numbers in range " +
-            "${LottoNumber.MINIMUM_NUMBER} to ${LottoNumber.MAXIMUM_NUMBER}.\n",
-    )
+	class InvalidAmountException(amount: Int) :
+		LottoException("Wrong amount: ${amount}, Amount should be at least ${Lotto.PRICE_OF_TICKET} ${Lotto.CURRENCY} and divisible by ${Lotto.PRICE_OF_TICKET}.\n")
 
-    class InvalidWinningNumbersFormatException(input: String) :
-        LottoException("Invalid winning numbers format: '$input'. Expected numbers separated by commas.")
+	class InvalidWinningNumbersFormatException(input: String) :
+		LottoException("Invalid winning numbers format: '$input'. Expected numbers separated by commas.")
 
-    class InvalidBonusNumberFormatException(input: String) :
-        LottoException(
-            "Invalid bonus number format: '$input'. Expected a number in the range of ${LottoNumber.MINIMUM_NUMBER} " +
-                "to ${LottoNumber.MAXIMUM_NUMBER} and different to the winning numbers.",
-        )
+	class InvalidWinningNumbersException(winningNumbers: List<Int>) :
+		LottoException("Invalid winning numbers: Expected ${Lotto.LOTTO_PICK_SIZE} numbers in range ${Lotto.MIN_RANGE} to ${Lotto.MAX_RANGE}\n")
 
-    class InvalidBonusNumberException(bonusNumber: LottoNumber) :
-        LottoException(
-            "Invalid bonus number: '$bonusNumber'. Expected a number in the range of ${LottoNumber.MINIMUM_NUMBER} " +
-                "to ${LottoNumber.MAXIMUM_NUMBER} and different to the winning numbers.",
-        )
+	class InvalidBonusNumberFormatException(input: String) :
+		LottoException("Invalid bonus number format: '$input'. Expected a number in the range of ${Lotto.MIN_RANGE} to ${Lotto.MAX_RANGE} and different to the winning numbers.")
 
-    class InvalidLottoNumberException(value: Int) : LottoException(
-        "Invalid lotto number: '$value'. Expected a number in the range of ${LottoNumber.MINIMUM_NUMBER} " +
-            "to ${LottoNumber.MAXIMUM_NUMBER}.",
-    )
+	class InvalidBonusNumberException(bonusNumber: Int) :
+		LottoException("Invalid bonus number format: '$bonusNumber'. Expected a number in the range of ${Lotto.MIN_RANGE} to ${Lotto.MAX_RANGE} and different to the winning numbers.")
 }

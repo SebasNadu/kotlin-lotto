ba940fa1597b930853a30b13159fd6b9be10cc40
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-07-06 21:12:17 +0200

    refactor(view): replace constants with private properties in InputView and OutputView

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,13 +1,18 @@
 package lotto.view
 
 import lotto.domain.Lotto
+import lotto.domain.LottoMachine
 import lotto.domain.Rank
 
 object OutputView {
+    private const val TICKETS_NUMBERS_PROMPT = "You have purchased"
+    private const val TITLE_OF_RESULT_PROMPT = "Winning Statistics\n------------------"
+    private const val TOTAL_RETURN_PROMPT = "Total return rate is"
+
     fun printTickets(tickets: List<Lotto>) {
         val numberOfTickets = tickets.size
         println(
-            "${Constants.TICKETS_NUMBERS_PROMPT} " +
+            "$TICKETS_NUMBERS_PROMPT " +
                 "$numberOfTickets ${pluralizeTicket(numberOfTickets)}.",
         )
         for (ticket in tickets) {
@@ -19,9 +24,9 @@ object OutputView {
         ranks: List<Rank>,
         returnRate: Double,
     ) {
-        println(Constants.TITLE_OF_RESULT_PROMPT)
+        println(TITLE_OF_RESULT_PROMPT)
         Rank.entries.filter { it != Rank.MISS }.reversed().forEach { printResultLine(it, ranks) }
-        println("${Constants.TOTAL_RETURN_PROMPT} ${"%.2f".format(returnRate)}")
+        println("$TOTAL_RETURN_PROMPT ${"%.2f".format(returnRate)}")
     }
 
     private fun printResultLine(
@@ -32,16 +37,10 @@ object OutputView {
         val hasBonus = if (entry.requiresBonus) " + Bonus Ball" else ""
         val winningMoney = "%,d".format(entry.winningMoney)
         val pluralizedTicket = pluralizeTicket(matchCount)
-        println("${entry.countOfMatch} Matches$hasBonus ($winningMoney ${Lotto.CURRENCY}) - $matchCount $pluralizedTicket")
+        println("${entry.countOfMatch} Matches$hasBonus ($winningMoney ${LottoMachine.CURRENCY}) - $matchCount $pluralizedTicket")
     }
 
     private fun pluralizeTicket(size: Int): String {
         return if (size == 1) "ticket" else "tickets"
-    }
-
-    object Constants {
-        const val TICKETS_NUMBERS_PROMPT = "You have purchased"
-        const val TITLE_OF_RESULT_PROMPT = "Winning Statistics\n------------------"
-        const val TOTAL_RETURN_PROMPT = "Total return rate is"
     }
 }

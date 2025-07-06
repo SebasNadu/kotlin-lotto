ecef8f8aadfb2f353aace1b2cf52f26a402d8cd0
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-07-06 15:16:41 +0200

    feat(analyzer): add ResultAnalyzer for calculating return rate of lotto results, removing it from LottoService

--- src/main/kotlin/lotto/domain/ResultAnalyzer.kt
+++ src/main/kotlin/lotto/domain/ResultAnalyzer.kt
@@ -1,11 +1,6 @@
 package lotto.domain
 
 object ResultAnalyzer {
-    fun evaluateTickets(
-        tickets: List<Lotto>,
-        winningCombination: WinningCombination,
-    ): List<Rank> = tickets.map { it.getRank(winningCombination) }
-
     fun calculateReturnRate(
         totalAmount: Int,
         results: List<Rank>,

a0a3dc39b66ec9dae65f387d4a4d3b48ef015f6a
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-07-06 16:19:36 +0200

    test(lotto): add unit tests Lotto .toString() and .getRank()

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -47,4 +47,36 @@ class LottoTest {
         val ticket = createLotto(1, 2, 3, 4, 5, 6)
         assertThat(ticket.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]")
     }
+
+    @Test
+    fun `should return FIRST rank when 6 numbers match`() {
+        val winning = createLotto(1, 2, 3, 4, 5, 6)
+        val player = createLotto(1, 2, 3, 4, 5, 6)
+        val winningCombination = WinningCombination(winning, LottoNumber.from(7))
+        assertThat(player.getRank(winningCombination)).isEqualTo(Rank.FIRST)
+    }
+
+    @Test
+    fun `should return SECOND rank when 5 match and bonus number`() {
+        val winning = createLotto(1, 2, 3, 4, 5, 6)
+        val player = createLotto(1, 2, 3, 4, 5, 7)
+        val winningCombination = WinningCombination(winning, LottoNumber.from(7))
+        assertThat(player.getRank(winningCombination)).isEqualTo(Rank.SECOND)
+    }
+
+    @Test
+    fun `should return THIRD rank when 5 match without bonus`() {
+        val winning = createLotto(1, 2, 3, 4, 5, 6)
+        val player = createLotto(1, 2, 3, 4, 5, 7)
+        val winningCombination = WinningCombination(winning, LottoNumber.from(8))
+        assertThat(player.getRank(winningCombination)).isEqualTo(Rank.THIRD)
+    }
+
+    @Test
+    fun `should return NONE when less than 3 match`() {
+        val winning = createLotto(1, 2, 3, 4, 5, 6)
+        val player = createLotto(7, 8, 9, 10, 11, 12)
+        val winningCombination = WinningCombination(winning, LottoNumber.from(13))
+        assertThat(player.getRank(winningCombination)).isEqualTo(Rank.MISS)
+    }
 }
